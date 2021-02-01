f41927e93414fbbe1b5ab4fb64c75a91e9c74645
lihanxiong <delegatevip@163.com>
2021-02-01 16:12:11 +0800

    wiki

---
+++ LocalizedStringDemo/LocalizedStringDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocalizedStringDemo
+//
+//  Created by delegate on 2021/2/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # iOSNSLocalizedString
 iOS多语言处理
+
+![avatar](operation.png)
+
+![avatar](effct1.png)
+
+![avatar](effect2.png)
